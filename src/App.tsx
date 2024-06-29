ea7697aef4b6aabde6efb5b670920d0b705a714b
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-06-30 01:49:22 +0600

    vercel.json added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import DarkModeBtn from './DarkModeBtn.tsx'
 export default function App() {
     return (
-        <main className="mt-20 max-w-[800px] mx-auto">
+        <main className="mt-20 max-w-[800px] mx-auto p-4">
             <h1 className="text-center font-bold text-3xl text-primary">
                 Todo<span className="text-black dark:text-white">Today!</span>
             </h1>
