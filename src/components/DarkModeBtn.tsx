47ecfd7866f9a11f5ccdfb3ccac05ca0c12f6723
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-07-02 14:28:21 +0600

    save darkmode state in localstorage

--- src/components/DarkModeBtn.tsx
+++ src/components/DarkModeBtn.tsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 import { FiMoon, FiSun } from "react-icons/fi";
 
 const DarkModeBtn = () => {
-    const [darkMode, setDarkMode] = useState(true);
+    const [darkMode, setDarkMode] = useState(() => {
+        const savedMode = localStorage.getItem("darkMode");
+        return savedMode ? JSON.parse(savedMode) : true;
+    });
 
     useEffect(() => {
         if (darkMode) {
@@ -10,12 +13,13 @@ const DarkModeBtn = () => {
         } else {
             document.documentElement.classList.remove("dark");
         }
+        localStorage.setItem("darkMode", JSON.stringify(darkMode));
     }, [darkMode]);
 
     return (
         <button
             onClick={() => setDarkMode(!darkMode)}
-            className="py-2 px-3 bg-black dark:bg-white text-white dark:text-black rounded-md"
+            className="py-2 px-3 bg-black dark:bg-white text-white dark:text-black rounded-md transition-colors duration-300"
         >
             {darkMode ? <FiSun size={24} /> : <FiMoon size={24} />}
         </button>
